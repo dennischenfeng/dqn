b2fec202459c6aa8bffe75b6fa153dba09e70260
dennis <dennisfeng2@gmail.com>
2021-05-31 09:56:00 -0700

    added __init__ files and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dqn',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='dennis',
+    author_email='',
+    description=''
+)
