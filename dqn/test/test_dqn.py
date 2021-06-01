fca567d8fa2c5f54e7b1e7e37e87735487499eeb
dennis <dennisfeng2@gmail.com>
2021-05-31 19:38:34 -0700

    added test_q_network test to test shape

--- dqn/test/test_dqn.py
+++ dqn/test/test_dqn.py
@@ -1,5 +1,11 @@
 import pytest
-from dqn.dqn import annealed_epsilon
+import torch as th
+from dqn.dqn import QNetwork, annealed_epsilon
+
+
+def generate_p_obs(minibatch_size):
+    # minibatch size 32, 4 channels, 84 x 84 image
+    return th.zeros((minibatch_size, 4, 84, 84))
 
 
 def test_annealed_epsilon():
@@ -11,4 +17,13 @@ def test_annealed_epsilon():
     assert annealed_epsilon(100, 1, 0.1, 200) == pytest.approx(0.55)
     assert annealed_epsilon(100, 1, 0, 200) == pytest.approx(0.5)
     assert annealed_epsilon(100, 0.5, 0, 200) == pytest.approx(0.25)
+
+
+def test_q_network():
+    for n_actions in [5, 10, 15]:
+        for minibatch_size in [32, 64, 128]:
+            q = QNetwork(n_actions)
+            p_obs = generate_p_obs(minibatch_size)
+            assert q(p_obs).shape == (minibatch_size, n_actions)
+
 
