ed3f5e824dc7aa32fd506a38c7ae6a55ccb238a8
dennis <dennisfeng2@gmail.com>
2021-06-01 19:23:38 -0700

    renamed to p_obs_seq

--- dqn/test/test_dqn.py
+++ dqn/test/test_dqn.py
@@ -4,7 +4,7 @@ import torch as th
 from dqn.dqn import QNetwork, ReplayMemory, annealed_epsilon
 
 
-def create_p_obs(minibatch_size):
+def create_p_obs_seq(minibatch_size):
     # minibatch size 32, 4 channels, 84 x 84 image
     return np.ones((minibatch_size, 4, 84, 84))
 
@@ -24,9 +24,9 @@ def test_q_network():
     for n_actions in [5, 10, 15]:
         for minibatch_size in [32, 64, 128]:
             q = QNetwork(n_actions)
-            p_obs = create_p_obs(minibatch_size)
-            p_obs = th.tensor(p_obs).float()
-            assert q(p_obs).shape == (minibatch_size, n_actions)
+            p_obs_seq = create_p_obs_seq(minibatch_size)
+            p_obs_seq = th.tensor(p_obs_seq).float()
+            assert q(p_obs_seq).shape == (minibatch_size, n_actions)
 
 
 def test_replay_memory_simple_obs():
@@ -88,26 +88,19 @@ def test_replay_memory_simple_obs():
 
 
 def test_replay_memory_representative_obs():
-    p_obs = create_p_obs(1)[0]
+    p_obs_seq = create_p_obs_seq(1)[0]
     action = 2
     rew = 50
-    p_obs2 = 2 * p_obs
+    p_obs2 = 2 * p_obs_seq
     done = False
 
-    m = ReplayMemory(10, p_obs.shape)
-    m.store(p_obs, action, rew, p_obs2, done)
+    m = ReplayMemory(10, p_obs_seq.shape)
+    m.store(p_obs_seq, action, rew, p_obs2, done)
 
-    po_s, a_s, r_s, po2_s, d_s = m.sample(1)
-    # import pdb; pdb.set_trace()
-    assert (po_s[0] == p_obs).all()
+    pos_s, a_s, r_s, pos2_s, d_s = m.sample(1)
+    assert (pos_s[0] == p_obs_seq).all()
     assert a_s[0] == action
     assert r_s[0] == rew
-    assert (po2_s[0] == p_obs2).all()
+    assert (pos2_s[0] == p_obs2).all()
     assert d_s[0] == done
-
-    # assert po_s[0] == np.expand_dims(p_obs, 0)).all()
-    # assert a_s == np.expand_dims(action, 0)).all()
-    # assert r_s == np.expand_dims(rew, 0)).all()
-    # assert po2_s == np.expand_dims(p_obs2, 0)).all()
-    # assert po2_s == np.expand_dims(p_obs2, 0
 
