cffef2958cd35ccb5b70ed1cc6d70fd60f880ff8
dennis <dennisfeng2@gmail.com>
2021-06-29 19:01:52 -0700

    save q network every n steps

--- dqn/callbacks.py
+++ dqn/callbacks.py
@@ -4,6 +4,7 @@ Callbacks to add additional functionality at specified points in learning algori
 
 from abc import ABC, abstractmethod
 from typing import Any
+import torch as th
 
 
 class BaseCallback(ABC):
@@ -31,13 +32,14 @@ class SaveQNetworkCallback(BaseCallback):
     Callback to save the Q network state dict of the model, after every training step
     """
 
-    def __init__(self, save_dir: str, save_prefix: str):
+    def __init__(self, save_freq: int, save_dir: str, save_prefix: str) -> None:
         """
         :param save_dir: directory to save the model's state dict
         :param save_prefix: prefix for saved file name. Full name will be
             `f"{save_prefix}_step{step_number}"`
         """
         super().__init__()
+        self.save_freq = save_freq
         self.save_dir = save_dir
         self.save_prefix = save_prefix
 
@@ -49,4 +51,8 @@ class SaveQNetworkCallback(BaseCallback):
         :param globals_:
         :return:
         """
-        # TODO: continue
+        step = locals_["self"].step
+        if step % self.save_freq == 0:
+            save_file = f"{self.save_dir}/{self.save_prefix}_step{step}"
+            q = locals_["self"].q
+            th.save(q.state_dict(), save_file)
