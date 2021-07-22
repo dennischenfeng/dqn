c0a819383adce35f92bd880645fb232293ebe5b5
dennis <dennisfeng2@gmail.com>
2021-07-22 09:04:53 -0700

    linted

--- dqn/replay_memory.py
+++ dqn/replay_memory.py
@@ -1,12 +1,23 @@
+"""
+Replay memory used to store transition data.
+"""
 
 import numpy as np
+from typing import Tuple
+import gym
 
 
-class ReplayMemory():
+class ReplayMemory:
     """
     Replay memory data buffer for storing past transitions
     """
-    def __init__(self, n, obs_shape, obs_dtype):
+
+    def __init__(self, n: int, obs_shape: Tuple[int, ...], obs_dtype: np.dtype) -> None:
+        """
+        :param n: storage capacity of replay memory (number of transitions)
+        :param obs_shape: shape of a signal transition's observation array
+        :param obs_dtype: datatype of elements in observation array
+        """
         n = int(n)
         self.n = n
         self.o = np.zeros((n, *obs_shape), dtype=obs_dtype)
@@ -16,7 +27,16 @@ class ReplayMemory():
         self.d = np.full(n, False)
         self.num_stores = 0
 
-    def store(self, obs, action, rew, obs2, done):
+    def store(self, obs: np.ndarray, action: int, rew: float, obs2: np.ndarray, done: bool) -> None:
+        """
+        Store a transition.
+
+        :param obs: observation
+        :param action: action
+        :param rew: reward
+        :param obs2: next observation
+        :param done: whether done
+        """
         i = self.num_stores % self.n
         self.o[i] = obs
         self.a[i] = action
@@ -25,17 +45,18 @@ class ReplayMemory():
         self.d[i] = done
         self.num_stores += 1
 
-    def sample(self, minibatch_size):
+    def sample(self, batch_size) -> Tuple[np.ndarray, int, float, np.ndarray, bool]:
         """
-        Sample a random minibatch of transitions
-        :return:
+        Sample a random minibatch of transitions (sampling with replacement)
+
+        :param batch_size: number of items in minibatch
         """
         active_storage_size = min(self.num_stores, self.n)
-        if minibatch_size > active_storage_size:
+        if batch_size > active_storage_size:
             raise ValueError(f"Not enough samples in replay memory ({active_storage_size}) to sample minibatch_size "
-                             f"({minibatch_size})")
+                             f"({batch_size})")
 
-        indices = np.random.randint(0, active_storage_size, size=minibatch_size)
+        indices = np.random.randint(0, active_storage_size, size=batch_size)
         o = self.o[indices]
         a = self.a[indices]
         r = self.r[indices]
@@ -45,17 +66,16 @@ class ReplayMemory():
         return o, a, r, o2, d
 
 
-def initialize_replay_memory(n_steps, env, replay_memory):
+def initialize_replay_memory(n_steps: int, env: gym.Env, replay_memory: ReplayMemory) -> None:
     """
     Initialize with random steps from env
 
-    :param n_steps:
-    :param env:
-    :param replay_memory:
-    :return:
+    :param n_steps: number of steps
+    :param env: gym environment
+    :param replay_memory: ReplayMemory object to initialize (will mutate)
     """
     obs = env.reset()
-    for step in range(n_steps):
+    for _ in range(n_steps):
         a = env.action_space.sample()
         obs2, r, d, _ = env.step(a)
 
